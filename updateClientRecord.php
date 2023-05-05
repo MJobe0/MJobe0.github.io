077398ad24b3c4cc658d51a3977c72fd9a79193f
MJobe-Blu <82389585+MJobe-Blu@users.noreply.github.com>
2023-05-05 15:58:07 +0100

    Create updateClientRecord.php

---
+++ updateClientRecord.php
@@ -0,0 +1,45 @@
+<?php
+
+// Get the webhook payload
+// $json = file_get_contents('php://input');
+// $data = json_decode($json);
+
+// Extract the field value from the payload
+// $field_value = $data->field_value;
+
+// Set up the Jotform API
+$api_key = 'bb3ac481fc42853b4acf9b822af03d63'; //Test remove later
+$team_id = "231156469084056"; //Test remove later
+$sub_id = '5589980639829119136'; //Test remove later
+$url = "https://innovativebc.jotform.com/API/submission/$sub_id?apiKey=$api_key"; //"https://api.jotform.com/form/$form_id/submissions";
+
+// Set up the API data
+$data = [
+  //'submission[FIELD_ID]' => $field_value,
+  "submission[5]" => "M",
+  "submission[6]" => "J",
+  "submission[55]" => "NO",
+  "submission[56]" => "YES",
+  "submission[57]" => "YES",
+  "submission[58]" => "Needs Assessment Pending"
+];
+
+// Send the API request
+$curl = curl_init();
+curl_setopt_array($curl, [
+  CURLOPT_URL => $url,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS => http_build_query($data),
+  CURLOPT_HTTPHEADER => [
+    "APIKEY: $api_key",
+    "jf-team-id: $team_id"
+  ],
+]);
+$response = curl_exec($curl);
+curl_close($curl);
+
+// Output the API response
+echo $response;
+
+?>
